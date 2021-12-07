b7bf2dd169dd79914a32aa5b487928edf654a457
Mercy <mercycherotich757@gmail.com>
2021-12-07 18:55:50 +0300

    add multiplication

--- js/script.js
+++ js/script.js
@@ -1,32 +1,47 @@
+//addition or sum
 var add = function(number1, number2) {
   return number1 + number2;
 };
-
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = add(number1, number2);
-alert(result);
+alert("The sum is " + result);
+
+//subtraction
+
 var subtract = function(number1, number2) {
   return number1 - number2;
 };
-
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = subtract(number1, number2);
-alert(result);
+alert("The subtraction output is " + result);
+
+//multiplication
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = multiply(number1, number2);
+alert("The multiplication output is " + result);
+
+
+//division
 var divide = function(number1, number2) {
   return number1 / number2;
 };
-
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = divide(number1, number2);
-alert(result);
+alert("The division output is " + result);
+
+//modulus
+
 var modulus = function(number1, number2) {
   return number1 % number2;
 };
-
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = modulus(number1, number2);
-alert(result);
+alert("The modulus is " + result);
